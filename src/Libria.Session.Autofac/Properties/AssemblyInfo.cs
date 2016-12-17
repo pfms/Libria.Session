c268521c345dc694e023c2a0a0dbfa46a81012d4
pfms <ita_jl@hotmail.com>
2016-12-17 19:23:20 +0000

    Changed how the nuget packages get generated

--- src/Libria.Session.Autofac/Properties/AssemblyInfo.cs
+++ src/Libria.Session.Autofac/Properties/AssemblyInfo.cs
@@ -6,30 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Libria.Session.Autofac")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Autofac extensions for Libria.Session library")]
 [assembly: AssemblyProduct("Libria.Session.Autofac")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7e4ccec6-8e9a-45b5-b127-c389c0fe0b9d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
